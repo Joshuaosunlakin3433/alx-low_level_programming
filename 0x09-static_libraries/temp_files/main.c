ca4b7fc76303f92ffb2574544f1959402f26e8b1
Joshuaosunlakin3433 <joshuaolaoluwa7@gmail.com>
2022-06-27 13:43:49 -0700

    main.c + betty issues

--- 0x09-static_libraries/temp_files/main.c
+++ 0x09-static_libraries/temp_files/main.c
@@ -1,7 +1,14 @@
 #include "main.h"
 
+/**
+ * main - this code prints the sentence below
+ *
+ * Return: nothing
+ */
+
 int main(void)
 {
 	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+
 	return (0);
 }
